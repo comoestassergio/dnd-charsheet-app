d6a42c64dc2dfdb99f79eae1568973b84c07bdef
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-25 18:16:57 +0300

    Add prop for padding

--- app/components/CategoryWrapper.tsx
+++ app/components/CategoryWrapper.tsx
@@ -4,13 +4,14 @@ interface CategoryWrapperProps {
     children: React.ReactNode | React.ReactNode [],
     title: string,
     button?: React.ReactNode,
+    pad?: string,
 }
 
 const CategoryWrapper: React.FC<CategoryWrapperProps> = ({
-    children, title, button
+    children, title, button, pad
 }) => {
     return (
-        <div className="relative border border-neutral-500 rounded-xl flex items-center justify-center py-6 px-5">
+        <div className={`relative border border-neutral-500 rounded-xl flex items-center justify-center ${pad ? pad : 'py-6 px-5' }`}>
             <div className="absolute -top-3 left-3 bg-white px-3">
                 <span className="font-bold text-md first-letter:capitalize">{title}</span>
             </div>
