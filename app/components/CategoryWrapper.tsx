db86bfc1baedfbd7973d1fdfb93e2a30fedb3c84
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-25 17:07:24 +0300

    Create category wrapper

---
+++ app/components/CategoryWrapper.tsx
@@ -0,0 +1,29 @@
+'use client';
+
+interface CategoryWrapperProps {
+    children: React.ReactNode | React.ReactNode [],
+    title: string,
+    button?: React.ReactNode,
+}
+
+const CategoryWrapper: React.FC<CategoryWrapperProps> = ({
+    children, title, button
+}) => {
+    return (
+        <div className="relative border border-neutral-500 rounded-xl h-20">
+            <div className="absolute -top-3 left-3 bg-white px-3">
+                <span className="font-bold text-md first-letter:capitalize">{title}</span>
+            </div>
+
+            {children}
+
+            {button && (
+                <div className="absolute -bottom-5 right-3">
+                    {button}
+                </div>
+            )}
+        </div>
+    )
+};
+
+export default CategoryWrapper;
