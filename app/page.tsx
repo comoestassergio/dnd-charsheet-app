6bbab0228b88645f87f39003e2a9c231a90bd474
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-25 18:55:50 +0300

    Create demo page

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,30 @@
+import CategoryWrapper from "./components/CategoryWrapper";
+import Divider from "./components/Divider";
+import StatBox from "./components/StatBox";
+import ChallengeItem from "./components/challenges/ChallengeItem";
+import ChallengesTable from "./components/challenges/ChallengesTable";
+import Challenges from "./components/layout/Challanges";
+import Skills from "./components/layout/Skills";
+import Stats from "./components/layout/Stats";
+import Vitals from "./components/layout/Vitals";
+import SkillGroup from "./components/skills/SkillGroup";
+import SkillItem from "./components/skills/SkillItem";
+import SkillsTable from "./components/skills/SkillsTable";
 
 
 export default function Home() {
+
   return (
-    <div></div>
+    <div className="p-5 flex flex-col gap-5">
+      <Vitals />
+      <Divider />
+      <div className="grid grid-cols-[1fr_6fr] gap-3">
+        <Stats />
+        <div className="flex flex-col gap-5">
+          <Challenges />
+          <Skills />
+        </div>
+      </div>
+    </div>
   )
 }
