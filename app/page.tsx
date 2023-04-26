4c47a38b816e833371696e905d703e992c322e23
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-26 22:00:44 +0300

    Update demo

--- app/page.tsx
+++ app/page.tsx
@@ -4,9 +4,12 @@ import StatBox from "./components/StatBox";
 import ChallengeItem from "./components/challenges/ChallengeItem";
 import ChallengesTable from "./components/challenges/ChallengesTable";
 import Challenges from "./components/layout/Challanges";
+import Inventory from "./components/layout/Inventory";
+import PlayerBio from "./components/layout/PlayerBio";
 import Skills from "./components/layout/Skills";
 import Stats from "./components/layout/Stats";
 import Vitals from "./components/layout/Vitals";
+import Weapons from "./components/layout/Weapons";
 import SkillGroup from "./components/skills/SkillGroup";
 import SkillItem from "./components/skills/SkillItem";
 import SkillsTable from "./components/skills/SkillsTable";
@@ -16,6 +19,15 @@ export default function Home() {
 
   return (
     <div className="p-5 flex flex-col gap-5">
+      <PlayerBio
+        name="Андрэ"
+        playerclass="Паладин"
+        level="4"
+        xp='3700'
+        origin="Благородное"
+        race="Полуэльф"
+        worldview="Доброе" 
+      />
       <Vitals />
       <Divider />
       <div className="grid grid-cols-[1fr_6fr] gap-3">
@@ -23,6 +35,8 @@ export default function Home() {
         <div className="flex flex-col gap-5">
           <Challenges />
           <Skills />
+          <Weapons />
+          <Inventory />
         </div>
       </div>
     </div>
