3f8bbabea0fa074ca2f91e647a86a8456ea63836
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-26 21:06:09 +0300

    Add margin bottom if theres a button

--- app/components/CategoryWrapper.tsx
+++ app/components/CategoryWrapper.tsx
@@ -11,7 +11,7 @@ const CategoryWrapper: React.FC<CategoryWrapperProps> = ({
     children, title, button, pad
 }) => {
     return (
-        <div className={`relative border border-neutral-500 rounded-xl flex items-center justify-center ${pad ? pad : 'py-6 px-5' }`}>
+        <div className={`relative border border-neutral-500 rounded-xl flex items-center justify-center ${pad ? pad : 'py-6 px-5' } ${button && 'mt-3'}`}>
             <div className="absolute -top-3 left-3 bg-white px-3">
                 <span className="font-bold text-md capitalize">{title}</span>
             </div>
