88323ead2b84e235a8f83574e490ad80dc8eea2c
Serge <93369654+comoestassergio@users.noreply.github.com>
2023-04-26 21:25:36 +0300

    Add header to layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,3 +1,4 @@
+import Header from './components/Header';
 import './globals.css'
 import { Nunito } from 'next/font/google'
 
@@ -15,7 +16,10 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en">
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Header />
+        {children}
+      </body>
     </html>
   )
 }
